209e98574e3462bae08c415033116189c1b8ffa6
LeeGyung-jin <0l055225757@gmail.com>
2022-08-10 05:07:12 +0900

    # Team>Landers - 더미 데이터 추가 - 데이터 등록 중 > imageDTOList null

--- src/main/java/com/bbgo/service/stadiumService/LandersServiceImpl.java
+++ src/main/java/com/bbgo/service/stadiumService/LandersServiceImpl.java
@@ -4,6 +4,8 @@ import com.bbgo.dto.common.PageRequestDTO;
 import com.bbgo.dto.common.PageResultDTO;
 import com.bbgo.dto.team.StadiumDTO;
 import com.bbgo.entity.Stadium;
+import com.bbgo.entity.StadiumImage;
+import com.bbgo.repository.StadiumImageRepository;
 import com.bbgo.repository.StadiumRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -11,7 +13,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -20,12 +25,9 @@ import java.util.function.Function;
 public class LandersServiceImpl implements LandersService{
 
     private final StadiumRepository stadiumRepository;
+    private final StadiumImageRepository imageRepository;
 
-    @Override
-    public Long register(StadiumDTO dto) {
-        return null;
-    }
-
+    // List
     @Override
     public PageResultDTO<StadiumDTO, Stadium> getList(PageRequestDTO requestDTO) {
         Pageable pageable = requestDTO.getPageable(Sort.by("sno").descending());
@@ -33,5 +35,24 @@ public class LandersServiceImpl implements LandersService{
         Function<Stadium, StadiumDTO> fn = (entity -> entityToDto(entity));
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    // Register
+    @Transactional
+    @Override
+    public Long register(StadiumDTO stadiumDTO) {
+        log.info("SI>stadiumDTO: " + stadiumDTO);
+
+        Map<String, Object> entityMap = dtoToEntity(stadiumDTO);
+        Stadium stadium = (Stadium) entityMap.get("stadium");
+        List<StadiumImage> stadiumImageList = (List<StadiumImage>) entityMap.get("imgList");
+
+        stadiumRepository.save(stadium);
+
+        stadiumImageList.forEach(stadiumImage -> {
+            imageRepository.save(stadiumImage);
+        });
+
+        return stadium.getSno();
     }
 }
